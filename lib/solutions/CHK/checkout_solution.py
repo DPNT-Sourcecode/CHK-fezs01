08c9c2e8b187619c80ba7eb2d723c5c44194097b
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 08:56:26 +0000

    Thu May 02 08:56:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,20 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
+    result = 0
+    price_table = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+    offer_price = {"A": (3, 130), "B": (2, 45)}
+    items_cart = {}
+
+    for sku in skus:
+        if sku not in price_table.keys():
+            return -1
+        items_cart[sku] = items_cart[sku] + 1
+
     pass
+
