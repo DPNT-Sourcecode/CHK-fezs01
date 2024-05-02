e8ec1a3060efa059d6c8a8ab80458cc0b9795d22
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 11:01:16 +0000

    Thu May 02 11:01:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,16 @@
 def checkout(skus):
     result = 0
     price_table = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    offer_price = {"A": [(3, 130), (5, 200)], "B": (2, 45), "E": (2, "B")}
+    offer_price = {"A": [(3, 130), (5, 200)], "B": (2, 45)}
+    free_item_offer = {"E": (2, "B")}
     items_cart = {}
 
     for sku in skus:
         if sku not in price_table.keys():
             return -1
         items_cart[sku] = items_cart.get(sku, 0) + 1
+
+    for 
 
     for name, quantity in items_cart.items():
         if name in offer_price.keys():
@@ -25,12 +28,18 @@ def checkout(skus):
 def calculate_price_for_item_with_offer(
     offer_price, price_table, name, quantity, items_cart
 ):
-    if isinstance(offer_price[name][1], str):
-        pass
+
 
     apply_offer_count = divmod(quantity, offer_price[name][0])
+    
+    if isinstance(offer_price[name][1],str) and apply_offer_count[0]>1:
+        discounted_item = items_cart[offer_price[name][1]]
+        apply_offer_count[0]>discounted_item
+        
+        
     items_price_with_offer = apply_offer_count[0] * offer_price[name][1]
     items_price_with_out_offer = price_table[name] * apply_offer_count[1]
     items_total_price = items_price_with_offer + items_price_with_out_offer
 
     return items_total_price
+
