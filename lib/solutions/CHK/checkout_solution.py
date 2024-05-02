310cf89a4368b39db80f9bfba27066bf782b60d5
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 13:07:56 +0000

    Thu May 02 13:07:56 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -65,7 +65,3 @@ def apply_free_item_offer(items_cart, free_item_offer):
                     break
                 items_cart[discount_item_name] -= 1
                 item_num_free -= 1
-
-
-
-
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -55,6 +55,3 @@ class TestCheckout:
 
     def test_checkout_buy_two_get_one_free_itself_with_six_item(self):
         assert checkout_solution.checkout("FFFFFF") == 40
-
-
-
