4415eeb014d2946c7644392cec65aa0e648bc512
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 09:56:26 +0000

    Thu May 02 09:56:26 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,6 +2,10 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout:
-    def test_checkout(self):
+    def test_checkout_with_empty_cart(self):
         assert checkout_solution.checkout("") == 0
+
+    def test_checkout_with_no_offer(self):
+        assert checkout_solution.checkout("ABC") == 100
+
 
