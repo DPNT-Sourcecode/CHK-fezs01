5332caee393bb20a326753e2403facf3fe30dfee
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 12:18:48 +0000

    Thu May 02 12:18:48 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -33,4 +33,11 @@ class TestCheckout:
         assert checkout_solution.checkout("AAACDEE") == 245
 
     def test_checkout_multi_buy_with_one_offer_applied(self):
-        assert checkout_solution.checkout("AAAAACEE") == 400
+        assert checkout_solution.checkout("AAAAAAACEE") == 400
+
+    def test_checkout_multi_buy_with_both_offer_applied_to_one_item(self):
+        assert checkout_solution.checkout("AAAAAAAACEE") == 430
+
+    def test_checkout_multi_buy_with_both_offer_applied_to_one_item_and_one_extra(self):
+        assert checkout_solution.checkout("AAAAAAAAACEE") == 480
+
