be90231a29be4e9109973f8607e0fd2dfa55b5f7
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 11:19:16 +0000

    Thu May 02 11:19:16 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,6 +11,9 @@ class TestCheckout:
     def test_checkout_Item_A_with_offer(self):
         assert checkout_solution.checkout("AAA") == 130
 
+    def test_checkout_buy_two_get_one_free(self):
+        assert checkout_solution.checkout("AAABCDEE") == 245
+
     def test_checkout_Mix_Item_with_and_with_offer(self):
         assert checkout_solution.checkout("AAAABCD") == 245
 
@@ -19,3 +22,4 @@ class TestCheckout:
 
     def test_checkout_invaild_input_lower_case(self):
         assert checkout_solution.checkout("AAAAbBCD") == -1
+
