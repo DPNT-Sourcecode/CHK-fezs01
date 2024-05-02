3cca9857169d2a762977cf21e26970c9a1c97263
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 12:13:16 +0000

    Thu May 02 12:13:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,3 @@
-import collections
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -53,13 +50,14 @@ def apply_free_item_offer(items_cart, free_item_offer):
                 or discount_item_name not in items_cart.keys()
             ):
                 continue
-            item_num_free = divmod(items_cart[name], discount.keys()[0])[0]
+            item_num_free = divmod(items_cart[name], count)[0]
 
             while item_num_free > 0:
                 if items_cart[discount_item_name] == 0:
                     break
                 items_cart[discount_item_name] -= 1
                 item_num_free -= 1
+
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,23 +2,23 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout:
-    # def test_checkout_invaild_input(self):
-    #     assert checkout_solution.checkout("AAAA-BCD") == -1
+    def test_checkout_invaild_input(self):
+        assert checkout_solution.checkout("AAAA-BCD") == -1
 
-    # def test_checkout_invaild_input_lower_case(self):
-    #     assert checkout_solution.checkout("AAAAbBCD") == -1
+    def test_checkout_invaild_input_lower_case(self):
+        assert checkout_solution.checkout("AAAAbBCD") == -1
 
-    # def test_checkout_with_empty_cart(self):
-    #     assert checkout_solution.checkout("") == 0
+    def test_checkout_with_empty_cart(self):
+        assert checkout_solution.checkout("") == 0
 
-    # def test_checkout_with_no_offer(self):
-    #     assert checkout_solution.checkout("ABC") == 100
+    def test_checkout_with_no_offer(self):
+        assert checkout_solution.checkout("ABC") == 100
 
-    # def test_checkout_Item_A_with_offer(self):
-    #     assert checkout_solution.checkout("AAA") == 130
+    def test_checkout_Item_A_with_offer(self):
+        assert checkout_solution.checkout("AAA") == 130
 
-    # def test_checkout_Mix_Item_with_and_with_offer(self):
-    #     assert checkout_solution.checkout("AAAABCD") == 245
+    def test_checkout_Mix_Item_with_and_with_offer(self):
+        assert checkout_solution.checkout("AAAABCD") == 245
 
     def test_checkout_buy_two_get_one_free(self):
         assert checkout_solution.checkout("AAABCDEE") == 245
@@ -31,3 +31,4 @@ class TestCheckout:
 
     def test_checkout_buy_two_get_one_free_no_B(self):
         assert checkout_solution.checkout("AAACDEE") == 245
+
