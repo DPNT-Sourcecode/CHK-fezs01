c13693dce92c9baa23f87b7d013ab01f71eaa4c7
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 11:22:16 +0000

    Thu May 02 11:22:16 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,6 +2,12 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout:
+    def test_checkout_invaild_input(self):
+        assert checkout_solution.checkout("AAAA-BCD") == -1
+
+    def test_checkout_invaild_input_lower_case(self):
+        assert checkout_solution.checkout("AAAAbBCD") == -1
+
     def test_checkout_with_empty_cart(self):
         assert checkout_solution.checkout("") == 0
 
@@ -11,15 +17,19 @@ class TestCheckout:
     def test_checkout_Item_A_with_offer(self):
         assert checkout_solution.checkout("AAA") == 130
 
-    def test_checkout_buy_two_get_one_free(self):
-        assert checkout_solution.checkout("AAABCDEE") == 245
-
     def test_checkout_Mix_Item_with_and_with_offer(self):
         assert checkout_solution.checkout("AAAABCD") == 245
 
-    def test_checkout_invaild_input(self):
-        assert checkout_solution.checkout("AAAA-BCD") == -1
+    def test_checkout_buy_two_get_one_free(self):
+        assert checkout_solution.checkout("AAABCDEE") == 245
 
-    def test_checkout_invaild_input_lower_case(self):
-        assert checkout_solution.checkout("AAAAbBCD") == -1
+    def test_checkout_buy_four_get_two_free(self):
+        assert checkout_solution.checkout("AAABBCDEEEE") == 325
+
+    def test_checkout_buy_five_get_two_free(self):
+        assert checkout_solution.checkout("AAABBCDEEEEE") == 365
+
+    def test_checkout_buy_two_get_one_free_no_B(self):
+        assert checkout_solution.checkout("AAACDEE") == 245
+
 
