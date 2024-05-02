0edef8ec4f783e8e2649f80fcdc53d45b09c7979
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 09:14:26 +0000

    Thu May 02 09:14:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,5 +16,12 @@ def checkout(skus):
             return -1
         items_cart[sku] = items_cart[sku] + 1
 
+    for name, quantity in items_cart.items():
+        if name in offer_price.keys():
+            apply_offer_count = divmod(quantity, offer_price[name][0])
+            items_price_with_offer = apply_offer_count[0] * offer_price[name][1]
+            items_price_with_out_offer = price_table[name] * apply_offer_count[name][1]
+
     pass
+
 
