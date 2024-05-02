cd1d17cb2e9f9527559047c74aeefee484fbf30c
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 09:53:26 +0000

    Thu May 02 09:53:26 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,9 +1,7 @@
-import sys
-
-sys.path.append("../lib")
-from ..lib.solutions.CHK import checkout_solution
+from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout:
     def test_checkout(self):
         assert checkout_solution.checkout("") == 0
+
