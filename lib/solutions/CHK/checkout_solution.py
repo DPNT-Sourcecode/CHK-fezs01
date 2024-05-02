ad85aaa8a0dbfa7eac2e464aeab84267da9ea49f
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 12:57:50 +0000

    Thu May 02 12:57:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -56,9 +56,14 @@ def apply_free_item_offer(items_cart, free_item_offer):
             # if name == discount_item_name and items_cart[discount_item_name] > count:
             #     item_num_free -= 1
 
+            if name == discount_item_name:
+                if items_cart[discount_item_name] == count:
+                    item_num_free = 0
+
             while item_num_free > 0:
                 if items_cart[discount_item_name] == 0:
                     break
                 items_cart[discount_item_name] -= 1
                 item_num_free -= 1
+
 
