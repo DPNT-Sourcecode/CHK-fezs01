9a5352843c05b6bbcf8a82628aa5da6d2125eb4d
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 10:04:15 +0000

    Thu May 02 10:04:15 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,6 +11,16 @@ class TestCheckout:
     def test_checkout_Item_A_with_offer(self):
         assert checkout_solution.checkout("AAA") == 130
 
+    def test_checkout_Mix_Item_with_and_with_offer(self):
+        assert checkout_solution.checkout("AAAABCD") == 245
+
+    def test_checkout_invaild_input(self):
+        assert checkout_solution.checkout("AAAA-BCD") == -1
+
+    def test_checkout_invaild_input_lower_case(self):
+        assert checkout_solution.checkout("AAAAbBCD") == -1
+
+
 
 
 
