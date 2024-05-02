d88fcacc613dc1b421bb9069307ece857e89a059
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 11:52:16 +0000

    Thu May 02 11:52:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -42,15 +42,18 @@ def apply_free_item_offer(items_cart, free_item_offer):
             print("NAME", name)
             print(count)
             print(discount_item_name)
-            discount_item_name = discount[discount.keys()[0]]  # B
 
-        if name not in items_cart.keys() or discount_item_name not in items_cart.keys():
-            continue
-        item_num_free = divmod(items_cart[name], discount.keys()[0])[0]
+            if (
+                name not in items_cart.keys()
+                or discount_item_name not in items_cart.keys()
+            ):
+                continue
+            item_num_free = divmod(items_cart[name], discount.keys()[0])[0]
 
-        while item_num_free > 0:
-            if items_cart[discount_item_name] == 0:
-                break
-            items_cart[discount_item_name] -= 1
-            item_num_free -= 1
+            while item_num_free > 0:
+                if items_cart[discount_item_name] == 0:
+                    break
+                items_cart[discount_item_name] -= 1
+                item_num_free -= 1
+
 
