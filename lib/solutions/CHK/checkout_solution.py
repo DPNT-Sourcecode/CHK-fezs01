12feb542b286b717e05c58f5b65e412a4d6b0be3
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 11:31:16 +0000

    Thu May 02 11:31:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,40 @@
 def checkout(skus):
     result = 0
     price_table = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    offer_price = {"A": (3, 130), "B": (2, 45)}
-    free_item_offer = {"E": (2, "B")}
+    multi_buy_price = {"A": {3: 130, 5: 200}, "B": {2: 45}}
+    free_item_offer = {"E": {2: "B"}}
     items_cart = {}
 
     for sku in skus:
         if sku not in price_table.keys():
             return -1
         items_cart[sku] = items_cart.get(sku, 0) + 1
-    print(items_cart)
+
+    apply_free_item_offer(items_cart, free_item_offer)
+
+    for name, quantity in items_cart.items():
+        if name in multi_buy_price.keys():
+            result += apply_multi_buy_offer(
+                multi_buy_price, price_table, name, quantity
+            )
+        else:
+            result += price_table[name] * quantity
+
+    return result
+
+
+def apply_multi_buy_offer(offer_price, price_table, name, quantity):
+
+    apply_offer_count = divmod(quantity, offer_price[name][0])
+
+    items_price_with_offer = apply_offer_count[0] * offer_price[name][1]
+    items_price_with_out_offer = price_table[name] * apply_offer_count[1]
+    items_total_price = items_price_with_offer + items_price_with_out_offer
+
+    return items_total_price
+
+
+def apply_free_item_offer(items_cart, free_item_offer):
     for name, discount in free_item_offer.items():
         discount_item_name = discount[1]
 
@@ -25,24 +50,4 @@ def checkout(skus):
             items_cart[discount_item_name] -= 1
             item_num_free -= 1
 
-    for name, quantity in items_cart.items():
-        if name in offer_price.keys():
-            result += calculate_price_for_item_with_offer(
-                offer_price, price_table, name, quantity
-            )
-        else:
-            result += price_table[name] * quantity
-
-    return result
-
-
-def calculate_price_for_item_with_offer(offer_price, price_table, name, quantity):
-
-    apply_offer_count = divmod(quantity, offer_price[name][0])
-
-    items_price_with_offer = apply_offer_count[0] * offer_price[name][1]
-    items_price_with_out_offer = price_table[name] * apply_offer_count[1]
-    items_total_price = items_price_with_offer + items_price_with_out_offer
-
-    return items_total_price
 
