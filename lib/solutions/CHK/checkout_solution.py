d9a92780f0d8e5c46e488ebea90206bb9545bbe9
sbx_user1051 <sbx_user1051@169.254.118.53>
2024-05-02 10:28:16 +0000

    Thu May 02 10:28:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,35 +1,78 @@
-# noinspection PyUnusedLocal
-# skus = unicode string
-def checkout(skus):
-    result = 0
-    price_table = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    offer_price = {"A": [(3, 130), (5, 200)], "B": (2, 45), "E": (2, "B")}
-    items_cart = {}
-
-    for sku in skus:
-        if sku not in price_table.keys():
-            return -1
-        items_cart[sku] = items_cart.get(sku, 0) + 1
-
-    for name, quantity in items_cart.items():
-        if name in offer_price.keys():
-            result += calculate_price_for_item_with_offer(
-                offer_price, price_table, name, quantity,items_cart
-            )
-        else:
-            result += price_table[name] * quantity
-
-    return result
+import sys
+from tdl.queue.queue_based_implementation_runner import (
+    QueueBasedImplementationRunnerBuilder,
+)
+from tdl.runner.challenge_session import ChallengeSession
+from solutions.SUM import sum_solution
+from solutions.HLO import hello_solution
+from solutions.ARRS import array_sum
+from solutions.IRNG import int_range
+from solutions.FIZ import fizz_buzz_solution
+from solutions.CHK import checkout_solution
+from solutions.CHL import checklite_solution
+from runner.utils import Utils
+from runner.user_input_action import get_user_input
 
 
-def calculate_price_for_item_with_offer(offer_price, price_table, name, quantity,items_cart):
-    if ()
-    
-    apply_offer_count = divmod(quantity, offer_price[name][0])
-    items_price_with_offer = apply_offer_count[0] * offer_price[name][1]
-    items_price_with_out_offer = price_table[name] * apply_offer_count[1]
-    items_total_price = items_price_with_offer + items_price_with_out_offer
+"""
+  ~~~~~~~~~~ Running the system: ~~~~~~~~~~~~~
+ 
+    From IDE:
+       Run this file from the IDE.
+ 
+    From command line:
+       PYTHONPATH=lib python lib/send_command_to_server.py
+ 
+    To run your unit tests locally:
+       PYTHONPATH=lib python -m pytest -q test/solution_tests/
+ 
+  ~~~~~~~~~~ The workflow ~~~~~~~~~~~~~
+ 
+    By running this file you interact with a challenge server.
+    The interaction follows a request-response pattern:
+         * You are presented with your current progress and a list of actions.
+         * You trigger one of the actions by typing it on the console.
+         * After the action feedback is presented, the execution will stop.
+ 
+    +------+-----------------------------------------------------------------------+
+    | Step | The usual workflow                                                    |
+    +------+-----------------------------------------------------------------------+
+    |  1.  | Run this file.                                                        |
+    |  2.  | Start a challenge by typing "start".                                  |
+    |  3.  | Read the description from the "challenges" folder.                    |
+    |  4.  | Locate the file corresponding to your current challenge in:           |
+    |      |   ./lib/solutions                                                     |
+    |  5.  | Replace the following placeholder exception with your solution:       |
+    |      |   raise NotImplementedError()                                         |
+    |  6.  | Deploy to production by typing "deploy".                              |
+    |  7.  | Observe the output, check for failed requests.                        |
+    |  8.  | If passed, go to step 1.                                              |
+    +------+-----------------------------------------------------------------------+
+ 
+    You are encouraged to change this project as you please:
+         * You can use your preferred libraries.
+         * You can use your own test framework.
+         * You can change the file structure.
+         * Anything really, provided that this file stays runnable.
+ 
+"""
 
-    return items_total_price
+runner = (
+    QueueBasedImplementationRunnerBuilder()
+    .set_config(Utils.get_runner_config())
+    .with_solution_for("sum", sum_solution.compute)
+    .with_solution_for("hello", hello_solution.hello)
+    .with_solution_for("array_sum", array_sum.compute)
+    .with_solution_for("int_range", int_range.generate)
+    .with_solution_for("fizz_buzz", fizz_buzz_solution.fizz_buzz)
+    .with_solution_for("checkout", checkout_solution.checkout)
+    .with_solution_for("checklite", checklite_solution.checklite)
+    .create()
+)
+
+ChallengeSession.for_runner(runner).with_config(
+    Utils.get_config()
+).with_action_provider(lambda: get_user_input(sys.argv[1:])).start()
+
 
 
