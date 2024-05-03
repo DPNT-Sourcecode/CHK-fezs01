df0a8a97d3b8b3e3d0d8109daf1eea043c81eeee
sbx_user1051 <sbx_user1051@169.254.135.117>
2024-05-03 12:56:15 +0000

    Fri May 03 12:56:15 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -58,3 +58,7 @@ class TestCheckout:
 
     def test_checkout_buy_three_get_one_free_itself_with_8_item(self):
         assert checkout_solution.checkout("UUUUUUUU") == 240
+
+    def test_one(self):
+        assert checkout_solution.checkout("UUUUUUUU") == 240
+
